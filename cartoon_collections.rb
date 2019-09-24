02dc16a0969158f375592ebce351a52341566165
Gene H Li <ghl234@nyu.edu>
2019-09-24 22:12:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,51 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index { |m, n|
+  puts "#{n+1}. #{m}\n"
+  }
+  
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  y = Array.new
+  array.map{|m|
+    m.concat("!")
+    m = m.capitalize
+    y.push(m)
+  }
+  y
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  boolVal = false
+  
+  c = 0
+  
+  while (c < array.length) do
+  
+  if (array[c].length > 4)
+    boolVal = true
+    
+  end
+    c+=1
+  end
+  
+  boolVal
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while (i < array.length) do
+    
+    if (cheese_types.include?(array[i]))
+      return array[i]
+    end
+    i+=1
+  end
+  return nil
+  
 end
