d313aee8585d3be2bc4655aea91b4a0f925ec6a5
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 20:54:27 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index { |value, index| 
+    puts "#{index + 1 } #{value}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  capital_arr = []
+  arr.map { |value| 
+    capital_arr << value.capitalize + "!"
+  }
+  capital_arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? { |value|
+    value.length > 4    
+  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  match_value = nil
+  arr.any? { |value|
+    if cheese_types.include?(value) 
+      match_value = value
+    end
+  }
+  match_value
 end
+
